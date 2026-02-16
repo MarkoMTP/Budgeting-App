1c502b3f7ec7565a24074abdbe0ce5c0b69add25
MarkoMTP <marko.mtp8@gmail.com>
2026-02-16 21:36:26 +0100

    added a mock of the passport authentication in the setup file

--- server/test/setup.js
+++ server/test/setup.js
@@ -1,7 +1,24 @@
 import dotenv from "dotenv";
+import { vi } from "vitest";
 
 // Load .env.test
 dotenv.config({ path: ".env.test" });
 
 // IMPORTANT: Prisma reads DATABASE_URL, not TEST_DATABASE_URL
 process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
+
+vi.mock("passport", () => {
+  const passportMock = {
+    initialize: () => (req, res, next) => next(),
+
+    authenticate: () => (req, res, next) => {
+      req.user = { id: "1", name: "test@example.com" };
+      next();
+    },
+  };
+
+  return {
+    default: passportMock,
+    ...passportMock,
+  };
+});
